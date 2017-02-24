6b04d57812070ee8394fce0fb9b9ed628958b5c1
sandro sviben <sandro.sviben@hotmail.com>
2017-02-24 11:58:18 +0100

    Kreiranje liste, ubacivanje elemenata u listu, prolazak kroz listu i ispis

---
+++ Liste/Liste/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liste
+{
+    class Customer
+    {
+        public int ID { get; set; }
+        public string Ime { get; set; }
+        public int Placa { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer()
+            {
+                ID = 10,
+                Ime = "Sandro",
+                Placa = 6000
+            };
+
+            Customer customer2 = new Customer()
+            {
+                ID = 11,
+                Ime = "Filip",
+                Placa = 9000
+            };
+
+            Customer customer3 = new Customer()
+            {
+                ID = 12,
+                Ime = "Hrvoje",
+                Placa = 15000
+            };
+
+            List<Customer> listCust = new List<Customer>();
+            listCust.Add(customer1);
+            listCust.Add(customer2);
+            listCust.Add(customer3);
+
+            foreach (Customer c in listCust)
+            {
+                Console.WriteLine($"ID = {c.ID}, Ime = {c.Ime}, Placa = {c.Placa}");
+            }
+
+          
+        }
+    }
+}
